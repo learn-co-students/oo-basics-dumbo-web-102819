f1e4bdbd939bfc9cff63284ba9e79cc97d03a949
Samir Jasarat <samirjasarat@gmail.com>
2019-10-29 15:10:44 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,36 @@
+class Book
+
+    def initialize(title)
+        @title = title
+    end 
+
+    def title
+        @title
+    end
+
+    ef author=(author)
+    @author = author
+  end
+ 
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+end
 
 
